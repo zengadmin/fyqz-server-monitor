d8a7806ab7bc80bb5cc070ba769327d75a7901c2
1158553513@qq.com <1158553513@qq.com>
2019-06-11 20:26:26 +0800

    Initial commit

---
+++ src/main/java/com/fyqz/FyqzServerMonitorApplication.java
@@ -0,0 +1,52 @@
+package com.fyqz;
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@SpringBootApplication
+@EnableAdminServer
+@EnableEurekaClient
+public class FyqzServerMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FyqzServerMonitorApplication.class, args);
+	}
+	@Configuration
+	public static class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
+		private final String adminContextPath;
+
+		public SecuritySecureConfig(AdminServerProperties adminServerProperties) {
+			this.adminContextPath = adminServerProperties.getContextPath();
+		}
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+			successHandler.setTargetUrlParameter("redirectTo");
+			successHandler.setDefaultTargetUrl(adminContextPath + "/");
+
+			http.authorizeRequests()
+					.antMatchers(adminContextPath + "/assets/**").permitAll()
+					.antMatchers(adminContextPath + "/login").permitAll()
+					.anyRequest().authenticated()
+					.and()
+					.formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()
+					.logout().logoutUrl(adminContextPath + "/logout").and()
+					.httpBasic().and()
+					.csrf()
+					.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+					.ignoringAntMatchers(
+							adminContextPath + "/instances",
+							adminContextPath + "/actuator/**"
+					);
+		}
+	}
+}
